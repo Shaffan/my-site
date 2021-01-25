93b8d87f7639d6be568d20587ea726d3214d7fe9
Stefan Horne <stefan.horne@virginexperiencedays.co.uk>
2021-01-25 19:36:11 +0000

    Install GA/Gtag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Layout.module.scss'
+import Gtag from './gtag'
 
 export default function Layout(props) {
   return (
     <div className={styles.container}>
+      <Gtag></Gtag>
       {props.children}
     </div>
   )
---
+++ components/gtag.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+const { GTAG_ID } = process.env
+
+export default function Gtag() {
+  const gtagScript = (
+    <script async src={'https://www.googletagmanager.com/gtag/js?id=' + GTAG_ID}></script>
+  )
+  return (
+    <Head>
+      {gtagScript}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+          window.dataLayer = window.dataLayer || []
+          function gtag() {
+            dataLayer.push(arguments)
+          }
+          gtag('js', new Date())
+        
+          gtag('config', 'G-7BXQZGTBPN')`
+        }}
+      ></script>
+    </Head>
+  )
+}
